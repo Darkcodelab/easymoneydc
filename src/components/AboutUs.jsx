54d10f71031ae66fc656ce9b6cb626197ed4a0b7
Darkcodelab <deepachakaravarthi434@gmail.com>
2021-03-13 20:33:53 +0530

    File Upload

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,6 +7,15 @@ import Tick from "../assets/images/tick.png";
 import Rocket from "../assets/images/rocket.png";
 import Vision from "../assets/images/vision.png";
 import Mission from "../assets/images/mission.png";
+import Excellence from "../assets/images/excellence.svg";
+import Empowerment from "../assets/images/Empowerment.svg";
+import Integrity from "../assets/images/Integrity.svg";
+import Responsibility from "../assets/images/Responsibility.svg";
+import Teamwork from "../assets/images/Teamwork.svg";
+import Commitment from "../assets/images/Commitment.svg";
+import Innovation from "../assets/images/Innovation.svg";
+import Transparency from "../assets/images/Transparency.svg";
+import CultureImg from "../assets/images/culture_img.svg";
 
 //CSS file(s)
 import "../assets/css/AboutUs.css";
@@ -78,6 +87,93 @@ const AboutUs = () => {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="living__wrapper">
+        <div className="living__hero">
+          <h2>Living Our Values</h2>
+          <p>Our beliefs distinguish us as a company</p>
+        </div>
+        <div className="living__cards">
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Innovation} alt="Innovation" />
+            </div>
+            <div className="card__text">
+              <p>Innovation</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Excellence} alt="Excellence" />
+            </div>
+            <div className="card__text">
+              <p>Excellence</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Integrity} alt="Integrity" />
+            </div>
+            <div className="card__text">
+              <p>Integrity</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Responsibility} alt="Responsibility" />
+            </div>
+            <div className="card__text">
+              <p>Responsibility</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon transparency-img">
+              <img src={Transparency} alt="Transparency" />
+            </div>
+            <div className="card__text">
+              <p>Transparency</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Teamwork} alt="Teamwork" />
+            </div>
+            <div className="card__text">
+              <p>Teamwork</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Commitment} alt="Commitment" />
+            </div>
+            <div className="card__text">
+              <p>Commitment</p>
+            </div>
+          </div>
+          <div className="living__card">
+            <div className="card__icon">
+              <img src={Empowerment} alt="Empowerment" />
+            </div>
+            <div className="card__text">
+              <p>Empowerment</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="culture__wrapper">
+        <div className="culture__img__container">
+          <img src={CultureImg} alt="Culture" />
+        </div>
+        <div className="culture__text__container">
+          <h2>Our Culture</h2>
+          <p>
+            EasyCash believes in simplifying people's lives, be it customers or
+            business owners. Our values represent a big part of out identity. we
+            believe that innovation is the mean to a greater future, that's why
+            our employees show a great deal of commitment and dedication,
+            working as a one unit to achieve our mutual goals.
+          </p>
         </div>
       </div>
     </div>
