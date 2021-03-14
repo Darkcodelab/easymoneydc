6fe4ee2345a6a041086ee04ba609a7c8eb214f35
Darkcodelab <deepachakaravarthi434@gmail.com>
2021-03-14 12:14:36 +0530

    File Upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Logo from "../assets/images/logo2.png";
 import facebook from "../assets/images/facebook.png";
 import instagram from "../assets/images/instagram.png";
 import linkedin from "../assets/images/linkedin.png";
 import Menu from "../assets/images/menu.svg";
 import { NavLink } from "react-router-dom";
+import LogoNav from "../assets/images/easycash.png";
 
 // CSS FILE
 import "../assets/css/Header.css";
 
 const Header = () => {
   let [isOpen, setIsOpen] = useState(false);
-  let handleMenuOpen = () => setIsOpen(!isOpen);
+  let handleMenuOpen = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
   let navClick = () => setIsOpen(false);
 
   return (
@@ -23,6 +27,17 @@ const Header = () => {
           </NavLink>
         </div>
         <ul className={isOpen ? "nav__ul nav__ul--active" : "nav__ul"}>
+          <li className="nav__li nav__li__logo">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="nav__link--active"
+              className="nav__link "
+              onClick={navClick}
+            >
+              <img src={LogoNav} alt="Logo" />
+            </NavLink>
+          </li>
           <li className="nav__li">
             <NavLink
               exact
@@ -78,11 +93,35 @@ const Header = () => {
               Contact Us
             </NavLink>
           </li>
+          <li>
+            <div className="nav__social">
+              <div className="social__link__a">
+                <a href="/" className="social__link">
+                  يبرع
+                </a>
+              </div>
+              <div className="social__link__a social__link__icon">
+                <a href="/" className="social__link">
+                  <img src={linkedin} alt="LinkedIn" />
+                </a>
+              </div>
+              <div className="social__link__a social__link__icon">
+                <a href="/" className="social__link">
+                  <img src={facebook} alt="facebook" />
+                </a>
+              </div>
+              <div className="social__link__a social__link__icon">
+                <a href="/" className="social__link">
+                  <img src={instagram} alt="instagram" />
+                </a>
+              </div>
+            </div>
+          </li>
         </ul>
         <div className="nav__menu" onClick={handleMenuOpen}>
           <img src={Menu} alt="Menu" />
         </div>
-        <div className="nav__social">
+        {/* <div className="nav__social">
           <div className="social__link__a">
             <a href="/" className="social__link">
               يبرع
@@ -103,7 +142,7 @@ const Header = () => {
               <img src={instagram} alt="instagram" />
             </a>
           </div>
-        </div>
+        </div> */}
       </nav>
     </header>
   );
